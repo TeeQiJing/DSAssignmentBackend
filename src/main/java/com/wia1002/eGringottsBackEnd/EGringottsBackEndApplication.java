388abb286d3e8ccd1bbe72d29aac9f51797f97b4
TeeQiJing <qijingtee1227@gmail.com>
2024-04-30 10:49:51 +0800

    Initial Commit

---
+++ src/main/java/com/wia1002/eGringottsBackEnd/EGringottsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.wia1002.eGringottsBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGringottsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGringottsBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wia1002/eGringottsBackEnd/EGringottsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wia1002.eGringottsBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGringottsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
