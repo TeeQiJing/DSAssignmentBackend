e520ed53b8de37faf59d5a3693aa86d9c0b32c23
TeeQiJing <qijingtee1227@gmail.com>
2024-05-04 20:17:32 +0800

    hahsjas

--- src/main/java/com/wia1002/eGringottsBackEnd/model/Account.java
+++ src/main/java/com/wia1002/eGringottsBackEnd/model/Account.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
